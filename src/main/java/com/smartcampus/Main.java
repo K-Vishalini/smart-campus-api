f4c99b9a6fd1b6ff54a821a69cfc2798f0dfb4b0
K-Vishalini <karunatharanvishalini@gmail.com>
2026-04-08 14:42:42 +0530

    Add main server entry point

---
+++ src/main/java/com/smartcampus/Main.java
@@ -0,0 +1,24 @@
+package com.smartcampus;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+
+public class Main {
+    public static final String BASE_URI = "http://localhost:8080/";
+
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig().packages("com.smartcampus");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) throws Exception {
+        final HttpServer server = startServer();
+        System.out.println("Smart Campus API running at " + BASE_URI + "api/v1");
+        System.out.println("Press ENTER to stop the server...");
+        System.in.read();
+        server.stop();
+    }
+}
