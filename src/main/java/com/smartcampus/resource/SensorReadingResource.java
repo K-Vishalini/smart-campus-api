4984675cc6c01dd3183f956d6bbfb04be7574cdd
K-Vishalini <karunatharanvishalini@gmail.com>
2026-04-08 17:19:28 +0530

    Add sensor readings sub-resource

---
+++ src/main/java/com/smartcampus/resource/SensorReadingResource.java
@@ -0,0 +1,52 @@
+package com.smartcampus.resource;
+
+import com.smartcampus.exception.SensorUnavailableException;
+import com.smartcampus.model.Sensor;
+import com.smartcampus.model.SensorReading;
+import com.smartcampus.store.DataStore;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class SensorReadingResource {
+
+    private final String sensorId;
+
+    public SensorReadingResource(String sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    @GET
+    public Response getReadings() {
+        Sensor sensor = DataStore.getSensors().get(sensorId);
+        if (sensor == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("{\"error\":\"Sensor not found\"}")
+                    .build();
+        }
+        List<SensorReading> readings = DataStore.getReadingsForSensor(sensorId);
+        return Response.ok(readings).build();
+    }
+
+    @POST
+    public Response addReading(SensorReading reading) {
+        Sensor sensor = DataStore.getSensors().get(sensorId);
+        if (sensor == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("{\"error\":\"Sensor not found\"}")
+                    .build();
+        }
+        if ("MAINTENANCE".equalsIgnoreCase(sensor.getStatus())) {
+            throw new SensorUnavailableException(
+                "Sensor " + sensorId + " is under maintenance.");
+        }
+        SensorReading newReading = new SensorReading(reading.getValue());
+        DataStore.getReadingsForSensor(sensorId).add(newReading);
+        sensor.setCurrentValue(newReading.getValue());
+        return Response.status(Response.Status.CREATED).entity(newReading).build();
+    }
+}
